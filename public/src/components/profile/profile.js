df5486b101a51590d8bbae735ca0161e27fa80d9
Isabela Cortes B <isabelacortesb@MacBook-Pro-de-Isabela.local>
2022-09-02 22:14:42 -0500

    Isabela c- Taller Tinder

---
+++ public/src/components/profile/profile.js
@@ -0,0 +1,74 @@
+class MyProfile extends HTMLElement {
+   
+    static get observedAttributes() {
+      return ['name', 'lastname', 'age', 'description', 'imagenperfil',"profesion", "ubicacion"];
+    }
+  
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+  
+    connectedCallback() {
+      this.render();
+    }
+  
+    attributeChangedCallback(propName, oldValue, newValue) {
+      this[propName] = newValue;
+      this.render();
+    }
+  
+    render() {
+      this.shadowRoot.innerHTML = `
+          <link rel="stylesheet" href="./src/components/profile/style.css">
+          <section>
+            
+          <div class= "card"> 
+
+            
+            
+            <img  class="imagenChat" src="./img/chat.png">
+            <img  class="imagenLogo" src="./img/logo.png">
+            <img  class="imagenAjustes" src="./img/ajustes.png">
+
+            
+          
+            
+            <img  class="imagenProfesion" src="./img/profesion.png">
+            <img  class="imagenUbicacion" src="./img/ubicacion.png">
+            
+            
+            <div class="textoSuperior" >
+              <h1 id="nombreUsuario">${this.name} ${this.lastname}, ${this.age}</h1>
+
+              <h2>${this.profesion}</h2>
+              <h2>${this.ubicacion}</h2>
+            </div> 
+
+            
+
+            <img id="ImagenPerfil" src="${this.imagenperfil}">
+
+            <img class="ImagenesOtras" src="./img/back.jpg">
+            <img class="ImagenesOtras"src="./img/cancel.jpg">
+            <img class="ImagenesOtras" src="./img/estrella.jpg">
+            <img class="ImagenesOtras"src="./img/corazon.jpg">
+            <img class="ImagenesOtras" src="./img/rayo.jpg">
+            
+            <p>${this.description}</p>
+
+            
+          
+          
+          </div>
+          </section>
+
+
+          `;
+    }
+  }
+  
+  customElements.define('my-profile', MyProfile);
+  export default MyProfile;
+  
+  
